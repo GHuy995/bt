8e0fd3703572cf56785800eb3a74aa4e11870de0
Administrator <Administrator@P207-26>
2024-09-15 16:56:14 +0700

    ud

--- src/Processor.java
+++ src/Processor.java
@@ -1,7 +1,21 @@
 
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+
+
 public class Processor {
     public static void main(String[] args) {
         StudentList studentList = new StudentList();
+        Scanner scanner = new Scanner(System.in);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd//mm/yyyy");
         
-    }
+        while (true){
+            System.out.println("1.Add new student");
+            System.out.println("2.Update");
+        }
+    
+    
+    
+    
+    }                                                                                              
 }
